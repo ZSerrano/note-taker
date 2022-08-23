d5f781b7ca31ef888ebbd9fe3279f9ee8590104a
Brometheus <zs.serrano1@gmail.com>
2022-08-22 22:54:38 -0500

    created server.js file and added most routes

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const dbData = require('./db/db.json');
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(dbData);
+  });
+
+app.post('/api/notes', (req,res) => {
+    res.json(`${req.method} request received!`);
+    
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
