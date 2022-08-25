ecb5ecd2393893295d4895cbeaeadd67fbc4e2a6
Brometheus <zs.serrano1@gmail.com>
2022-08-24 19:50:36 -0500

    finishing touches

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const dbData = require('./db/db.json');
+const dbData = require('../Develop/db/db.json');
 const PORT = 3001;
 const app = express();
 
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -23,7 +23,23 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req,res) => {
     res.json(`${req.method} request received!`);
-    
+
+    const {title, text} = req.body
+
+    if (title && text) {
+    const newNote = {
+      title,
+      text,
+    }
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
 })
 
 app.listen(PORT, () =>
